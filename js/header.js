25b9eaf7016c7a4126c1ff760bef1a0e14927c56
hawks.jamesf <hawks.jamesf@gmail.com>
2018-02-11 18:14:24 +0800

    增加tag功能
    - 增加tag动态文章计数
    - 提取home界面代码

--- js/header.js
+++ js/header.js
@@ -1,7 +1,11 @@
-$(window).scroll(function() {
+$(window).scroll(function () {
   if ($(document).scrollTop() > 50) {
     $('nav').addClass('shrink');
   } else {
     $('nav').removeClass('shrink');
   }
+});
+
+$(document).ready(function () {
+  $('[data-toggle="tooltip"]').tooltip();
 });

26dbf0d2e919966ef7b510169e8c4782bbe4fe78
hawks.jamesf <hawks.jamesf@gmail.com>
2020-08-09 11:53:46 +0800

    modify ci config

--- before_deploy.sh
+++ before_deploy.sh
@@ -1,4 +1,4 @@
-# openssl aes-256-cbc -K $encrypted_4c7563b9ab37_key -iv $encrypted_4c7563b9ab37_iv -in id_rsa.enc -out ~/.ssh/id_rsa -d
-# chmod 600 ~/.ssh/id_rsa
-# echo -e "Host 101.200.36.88\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+openssl aes-256-cbc -K $encrypted_4c7563b9ab37_key -iv $encrypted_4c7563b9ab37_iv -in id_rsa.enc -out ~/.ssh/id_rsa -d
+chmod 600 ~/.ssh/id_rsa
+echo -e "Host 101.200.36.88\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 git submodule update --init --recursive
